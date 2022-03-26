deb17fed43a44296fb5e64412c24135262c5cc8e
Emily Dokken <dokkenemily@gmail.com>
2022-03-26 15:43:21 -0700

    add initial folder structure and header / nav components with style

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Navigation/index';
+
+function Header() {
+  return (
+    <header>
+      <h1>Emily Dokken</h1>
+      <Nav></Nav>
+    </header>
+  )
+}
+
+export default Header;
