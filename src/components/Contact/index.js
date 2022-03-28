65f4f78e8eb5f0bd77919fe11f726a82b7b41c6c
Emily Dokken <dokkenemily@gmail.com>
2022-03-27 17:51:04 -0700

    add form submit functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
-  return (
-      <form className="form">
-        <div className="name">
-          <label for="name">Name:</label>
-          <input type="text" name="name" className='inputName' tabIndex="1"></input>
-        </div>
-        <div className="email">
-          <label for="email">Email:</label>
-          <input type="email" name="email" id="email" className='inputEmail' placeholder='example@example.com' tabIndex="2"></input>
-        </div>
-        <div>
-          <label for="message">Message:</label>
-          <textarea placeholder='Start typing...' className='inputMessage' name="message"></textarea>
-        </div>
-        <button type="submit" className="submit">Submit</button>
-      </form>
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const { name, email, message } = formState;
 
-  )
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <form id="contact-form" className="form" onSubmit={handleSubmit}>
+      <div className="name">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          defaultValue={name}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div className="email">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          defaultValue={email}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          placeholder="Start typing..."
+          name="message"
+          defaultValue={message}
+          onChange={handleChange}
+        ></textarea>
+      </div>
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
 
-export default Contact
+export default Contact;
