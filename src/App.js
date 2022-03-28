1d4b54103b96dc8fe6c79e5c56f4b0a3367dcfea
Emily Dokken <dokkenemily@gmail.com>
2022-03-27 19:29:05 -0700

    add render states

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
@@ -7,14 +8,34 @@ import Resume from './components/Resume';
 import './index.css';
 
 function App() {
+  const [pages] = useState([
+    'About',
+    'Portfolio',
+    'Contact',
+    'Resume',
+  ]);
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
     <div className='root-child'>
-      <Header></Header>
+      <Header
+        pages={pages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      ></Header>
       <main>
-        {/* <About></About> */}
-        {/* <Portfolio></Portfolio> */}
-        {/* <Contact></Contact> */}
-        <Resume></Resume>
+        {currentPage === 'Portfolio' ? (
+          <Portfolio></Portfolio>
+        ) : <About></About>
+        }
+        {currentPage === 'Contact' ? (
+          <Contact></Contact>
+        ) : <About></About>
+        }
+        {currentPage === 'Resume' ? (
+          <Resume></Resume>
+        ) : <About></About> 
+        }
       </main>
       <Footer></Footer>
     </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import Nav from '../Navigation/index';
-import 'font-awesome/css/font-awesome.min.css'
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
 
-function Header() {
+function Header(props) {
+  const {
+    pages = [],
+    setCurrentPage,
+    currentPage,
+  } = props;
+
   return (
     <header>
       <h1 className="site-title">Emily Dokken</h1>
-      <Nav></Nav>
+      <nav>
+        <ul>
+          <li>
+            <a
+              href='#about'
+              onclick={() => setCurrentPage(pages)}
+              >About
+            </a>
+          </li>
+          <li>            
+            <a 
+              href='#portfolio'
+              onclick={() => setCurrentPage(pages)}
+              >Portfolio
+            </a>
+          </li>
+          <li>            
+            <a 
+              href='#contact'
+              onclick={() => setCurrentPage(pages)}
+              >Contact
+            </a>
+          </li>
+          <li>            
+            <a 
+              href='#about'
+              onclick={() => setCurrentPage(pages)}
+              >Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Header;
