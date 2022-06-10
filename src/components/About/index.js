147d6440fa2d7452e882fb03613efabf3cc9de7b
Emily Dokken <dokkenemily@gmail.com>
2022-06-09 23:34:50 -0700

    update about text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ function About() {
       <div className='about-text'>
         <h2>Fullstack Web Developer | Senior QA Engineer</h2>
         <p>
-        Senior QA Engineer turned fullstack web developer leveraging ability to critically problem solve and locate issues within code to build better products for an intuitive user experience.A recent graduate of the University of Oregon's Full Stack Javascript Web Development Bootcamp and on the hunt for a FullStack Web Development position.
+         Emily is a Senior QA Engineer turned fullstack web developer leveraging ability to critically problem solve and locate issues within code to build better products for an intuitive user experience. She recently graduated from the University of Oregon's Full Stack Javascript Web Development Bootcamp and is currently searching for a Full Stack or Frontend Development position. As a QA Engineer she has been exposed to Agile processes, worked in large development teams for Enterprise software companies, and has spent numerous hours debugging software with developers.
         </p>
       </div>
     </section>
